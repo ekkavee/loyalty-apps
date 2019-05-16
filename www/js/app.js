10b3b7ab3da03c17847a4cfce29130c96c5ff2e2
Ekkavee Veeratianpinyo <ekkavee@vectron.com.au>
2019-05-16 12:13:31 +1000

    ionic rating injecting to angularjs

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic', 'starter.controllers', 'app.services', 'app.directives', 'app.filters', 'ngCordovaOauth', 'ngTwitter', 'angularMoment', 'slickCarousel', 'barcode', 'ngFileUpload', 'ion-google-autocomplete', 'credit-cards', 'ionicLazyLoad', 'ngInputCurrency', 'ion-datetime-picker', 'angularLazyImg'])
+angular.module('starter', ['ionic', 'starter.controllers', 'app.services', 'app.directives', 'app.filters', 'ngCordovaOauth', 'ngTwitter', 'angularMoment', 'slickCarousel', 'barcode', 'ngFileUpload', 'ion-google-autocomplete', 'credit-cards', 'ionicLazyLoad', 'ngInputCurrency', 'ion-datetime-picker', 'angularLazyImg', 'ionic-ratings'])
 
   .run(function ($ionicPlatform, $rootScope, AuthService, $state, $timeout, MsgService, GeofenceService, MenuBadge, $window, Layout) {
     $ionicPlatform.ready(function () {
